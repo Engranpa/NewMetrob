c7ab8aa802dfbda683e46b7cad97e9b93092d70d
Engranpa <l.p.2.c_s@hotmail.com>
2020-04-05 20:00:05 -0300

    MovimientoBasico

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float Horizontalmove;
+    private float Verticalmove;
+    public CharacterController player;
+
+    public float playerSpeed;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontalmove = Input.GetAxis("Horizontal");
+        Verticalmove = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        player.Move(new Vector3(Horizontalmove, 0, Verticalmove) * playerSpeed);
+    }
+}
